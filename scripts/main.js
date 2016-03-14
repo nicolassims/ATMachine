4fa46f3c2d7c22df77d404683874f6e1b3f475e9
Emily Dudd <dudderkinz123@gmail.com>
2016-03-14 15:09:15 -0400

    Progressing nicely.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,25 @@
 /**
  * Created by Administrator on 3/14/2016.
  */
+
+'use strict';
+
+import HideStuff from './HideStuff';
+import RevealStuff from './RevealStuff';
+import AddNames from './AddNames';
+
+class main {
+    constructor() {
+
+        new main.useATM();
+    }
+
+    static useATM() {
+
+        return main.useATM();
+    }
+}
+
+window.onload = function() {
+    new main();
+};
