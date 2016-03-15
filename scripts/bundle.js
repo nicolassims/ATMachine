9aae771869723ac05e98ee292edb0b875ecca026
Emily Dudd <dudderkinz123@gmail.com>
2016-03-15 15:10:23 -0400

    Progressing nicely.

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -42,7 +42,7 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ function(module, exports) {
 
 	/**
 	 * Created by Administrator on 3/14/2016.
@@ -51,20 +51,6 @@
 	'use strict';
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-	var _HideStuff = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./HideStuff\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
-
-	var _HideStuff2 = _interopRequireDefault(_HideStuff);
-
-	var _RevealStuff = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./RevealStuff\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
-
-	var _RevealStuff2 = _interopRequireDefault(_RevealStuff);
-
-	var _AddNames = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./AddNames\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
-
-	var _AddNames2 = _interopRequireDefault(_AddNames);
-
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -76,9 +62,27 @@
 	    }
 
 	    _createClass(main, null, [{
-	        key: 'useATM',
+	        key: "useATM",
 	        value: function useATM() {
-
+	            var request = new XMLHttpRequest();
+	            request.open("GET", filePath, true);
+	            request.send();
+	            request.onload = function () {
+	                var COLUMNS = 2;
+	                var cardNumber = void 0,
+	                    PIN = [];
+	                if (request.readyState === 4 && request.status === 200) {
+	                    data = request.responseText.split(/\n/);
+	                }
+	                for (var i = 0; i < data.length; i++) {
+	                    middleData = data[i].split(/,/);
+	                    finalData[i] = []; //makes it an MD array
+	                    for (var j = 0; j < COLUMNS; j++) {
+	                        finalData[i][j] = middleData[j];
+	                    }
+	                }
+	                callback(finalData);
+	            };
 	            return main.useATM();
 	        }
 	    }]);
--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,6 @@
 
 'use strict';
 
-import HideStuff from './HideStuff';
-import RevealStuff from './RevealStuff';
-import AddNames from './AddNames';
-
 class main {
     constructor() {
 
@@ -15,7 +11,24 @@ class main {
     }
 
     static useATM() {
-
+        let request = new XMLHttpRequest();
+        request.open("GET", filePath, true);
+        request.send();
+        request.onload = function() {
+            const COLUMNS = 2;
+            let cardNumber, PIN = [];
+            if (request.readyState === 4 && request.status === 200) {
+                data = request.responseText.split(/\n/);
+            }
+            for (let i = 0; i < data.length; i++) {
+                middleData = data[i].split(/,/);
+                finalData[i] = []; //makes it an MD array
+                for (let j = 0; j < COLUMNS; j++) {
+                    finalData[i][j] = middleData[j];
+                }
+            }
+            callback(finalData);
+        };
         return main.useATM();
     }
 }
